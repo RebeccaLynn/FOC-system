02b1f634baafe4371044993562ea5cbfc9e52dc4
kbrobbey <kbrobbey@umw.edu>
2013-03-18 22:38:10 -0400

    Revert "Revert "Initial Database Connection""
    This reverts commit 6447e9780e7338d8dd5a3f687dc3d997eaba5b16.

--- UserModel/src/usermodel/Main.java
+++ UserModel/src/usermodel/Main.java
@@ -4,7 +4,7 @@
  */
 
 package usermodel;
-
+import java.sql.*;
 /**
  *
  * @author Rebecca
@@ -14,8 +14,33 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+     static Statement stmt = null;
+     static ResultSet rs = null;
+    
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+         try {
+             Connect conn = new Connect();
+             System.out.println("Connection established");
+             stmt = conn.conn.createStatement();
+             rs = stmt.executeQuery("SELECT * FROM members");
 
+    // or alternatively, if you don't know ahead of time that
+    // the query will be a SELECT...
+
+           if (stmt.execute("SELECT * FROM members")) {
+               rs = stmt.getResultSet();
+               while ( rs.next() ){
+                   System.out.println(rs.getString("fname") + " " + rs.getString("lname"));
+               } 
+				
+            }       
+         }
+         catch (SQLException e) {
+             e.printStackTrace();
+             System.err.println("Connection Failure");
+         }  
+        
+    }
+    
 }
